0db5e78de86908269af2f6530462177f4632cb77
SmitParikh1234 <72654695+SmitParikh1234@users.noreply.github.com>
2021-08-24 08:01:25 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,18 @@
+import * as React from 'react';
+import { Text, View, StyleSheet } from 'react-native';
+
+import { NavigationContainer } from '@react-navigation/native';
+import DrawerNavigator from './navigation/DrawerNavigation'
+
+
+
+export default function App() {
+  return (
+    <NavigationContainer>
+     <DrawerNavigator/>
+    </NavigationContainer>
+  );
+}
+
+
+
---
+++ screens/Profile.js
@@ -0,0 +1,13 @@
+import *as React from 'react'
+import {Text,View,StyleSheet} from "react-native"
+
+export default class CreatePost extends React.Component{
+  render(){
+    return(
+      <View style={{flex:1,justifyContent:"cenetr",}}>
+      <Text >Profile</Text>
+      </View>
+    )
+  }
+
+}
